8cea5eebad27b6f2ed01cb35882b89ad48ab540d
Marcus Grant <marcusfg@gmail.com>
2021-01-18 22:20:12 +0100

    fix: Add missing DndProvider to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend as Backend } from 'react-dnd-html5-backend';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppStateProvider } from './AppStateContext';
 import './index.css';
 
 ReactDOM.render(
+  <DndProvider backend={Backend}>
   <AppStateProvider>
     <App />
-  </AppStateProvider>,
+    </AppStateProvider>
+  </DndProvider>,
   document.getElementById('root')
 );
 
